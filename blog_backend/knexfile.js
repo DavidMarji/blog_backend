568248ac9290796c6cfae6c220da5e042c9f5855
DavidMarji <david.marji@mail.mcgill.ca>
2024-07-18 12:12:35 +0300

    added production env settings on prod env db

--- blog_backend/knexfile.js
+++ blog_backend/knexfile.js
@@ -16,6 +16,19 @@ module.exports = {
     migrations: {
       directory: './models/migrations'
     }
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+      ssl: { rejectUnauthorized: false }
+    },
+    migrations: {
+      directory: './migrations'
+    },
   }
-
 };
